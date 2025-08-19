f36126b53d6bf164fd3adabf0d0978a2f108f19d
GabsCavalcant <gabrielnina22@gmail.com>
2025-08-19 19:21:23 -0300

    Projeto criado

---
+++ src/test/java/com/gabsprojeto/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabsprojeto.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
