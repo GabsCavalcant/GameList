9af44d304b465c835bd6f92c62c56b52f456a2ce
GabsCavalcant <gabrielnina22@gmail.com>
2025-08-22 21:20:41 -0300

    Game Get Pelo Id

--- src/main/java/com/gabsprojeto/gamelist/controlers/GameController.java
+++ src/main/java/com/gabsprojeto/gamelist/controlers/GameController.java
@@ -3,9 +3,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabsprojeto.gamelist.dto.GameDto;
 import com.gabsprojeto.gamelist.dto.GameMinDto;
 import com.gabsprojeto.gamelist.services.GameService;
 
@@ -15,6 +17,15 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}") 
+	public GameDto findById(@PathVariable Long id){
+		GameDto result =  gameService.findById(id);
+		return result;
+		
+	}
+	
+	
 	
 	@GetMapping
 	public List<GameMinDto> findAll(){
--- src/main/java/com/gabsprojeto/gamelist/services/GameService.java
+++ src/main/java/com/gabsprojeto/gamelist/services/GameService.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gabsprojeto.gamelist.dto.GameDto;
 import com.gabsprojeto.gamelist.dto.GameMinDto;
 import com.gabsprojeto.gamelist.entities.Game;
 import com.gabsprojeto.gamelist.repositories.GameRepository;
@@ -18,6 +20,13 @@ public class GameService {
 	@Autowired //injetar/ o propio spring resolve 
 	private GameRepository gameRepository;
 	
+	
+	@Transactional(readOnly = true)
+	public GameDto findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDto(result);
+		
+	}
 
 	public List<GameMinDto> findAll(){
 		List<Game> resultado = gameRepository.findAll();
